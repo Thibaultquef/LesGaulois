fbb885b1585f90b52239db8503459436c1d14cb9
ManiaHyt <queffelecthibault@gmail.com>
2024-03-10 20:21:11 +0100

    Correction classe Armure

--- src/personnages/Armures.java
+++ src/personnages/Armures.java
@@ -1,12 +1,19 @@
 package personnages;
 
 public enum Armures {
-	Bouclier(3), Casque(2), Plastron(3);
-
+	Bouclier("Bouclier", 3), Casque("Casque", 2), Plastron("Plastron", 3);
+	
+	
+	private final String nom;
 	private final int protection;
 
-	Armures(int protection) {
+	Armures(String nom, int protection) {
+		this.nom = nom;
 		this.protection = protection;
+	}
+	
+	public String getnom(){
+		return nom;
 	}
 
 	public int getProtection() {
